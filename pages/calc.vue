3b2aac07d09e442cfcf8869ffd7492c929f1cb84
Yugendiran <yyugi64@gmail.com>
2023-12-01 21:07:06 +0530

    .

---
+++ pages/calc.vue
@@ -0,0 +1,77 @@
+<template>
+  <div>
+    <div class="flex items-center justify-center h-screen bg-gray-200">
+      <div class="p-8 bg-white rounded shadow-md">
+        <div class="grid grid-cols-4 gap-4">
+          <div class="col-span-4 mb-4">
+            <input
+              v-model="inputValues"
+              type="text"
+              class="w-full p-2 border border-gray-300 rounded"
+              readonly
+            />
+          </div>
+          <button class="col-span-1 btn" @click="addValue('7')">7</button>
+          <button class="col-span-1 btn" @click="addValue('8')">8</button>
+          <button class="col-span-1 btn" @click="addValue('9')">9</button>
+          <button class="col-span-1 bg-yellow-500 btn" @click="addValue('/')">
+            /
+          </button>
+
+          <button class="col-span-1 btn" @click="addValue('4')">4</button>
+          <button class="col-span-1 btn" @click="addValue('5')">5</button>
+          <button class="col-span-1 btn" @click="addValue('6')">6</button>
+          <button class="col-span-1 bg-yellow-500 btn" @click="addValue('*')">
+            *
+          </button>
+
+          <button class="col-span-1 btn" @click="addValue('1')">1</button>
+          <button class="col-span-1 btn" @click="addValue('2')">2</button>
+          <button class="col-span-1 btn" @click="addValue('3')">3</button>
+          <button class="col-span-1 bg-yellow-500 btn" @click="addValue('-')">
+            -
+          </button>
+
+          <button class="col-span-1 btn" @click="addValue('0')">0</button>
+          <button class="col-span-1 btn" @click="addValue('.')">.</button>
+          <button class="col-span-1 bg-yellow-500 btn" @click="calc()">
+            =
+          </button>
+          <button class="col-span-1 bg-yellow-500 btn" @click="addValue('+')">
+            +
+          </button>
+
+          <button class="col-span-2 bg-red-500 btn" @click="inputValues = ''">
+            Clear
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      inputValues: "",
+    };
+  },
+  methods: {
+    addValue(value) {
+      this.inputValues += value;
+    },
+    calc() {
+      try {
+        if (eval(this.inputValues)) {
+          this.inputValues = eval(this.inputValues);
+        } else {
+          alert("Please enter a valid calculation");
+        }
+      } catch {
+        alert("Please enter a valid calculation");
+      }
+    },
+  },
+};
+</script>
---
+++ pages/card-flip-game.vue
@@ -0,0 +1,74 @@
+<template>
+  <div
+    class="grid items-center justify-center w-full min-h-screen grid-cols-5 bg-yellow-500"
+  >
+    <div
+      v-for="(card, index) in cards"
+      class="flex items-center justify-center w-20 h-20 text-3xl font-bold bg-white rounded-xl"
+      @click="openCard(index)"
+    >
+      <p v-if="card.isFlipped">{{ card.value }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      cards: [],
+      openedCard: {
+        value1: null,
+        value2: null,
+      },
+    };
+  },
+  mounted() {
+    for (let i = 0; i < 9; i++) {
+      let randomNum = Math.floor(Math.random() * 99) + 1;
+
+      this.cards.push({
+        value: randomNum,
+        isFlipped: false,
+      });
+
+      this.cards.push({
+        value: randomNum,
+        isFlipped: false,
+      });
+    }
+
+    this.cards.sort(() => Math.random() - 0.5);
+  },
+  methods: {
+    openCard(index) {
+      if (this.openedCard.value1 === null) {
+        this.openedCard.value1 = this.cards[index].value;
+        this.cards[index].isFlipped = true;
+      } else if (this.openedCard.value2 === null) {
+        this.openedCard.value2 = this.cards[index].value;
+        this.cards[index].isFlipped = true;
+
+        setTimeout(() => {
+          if (this.openedCard.value1 == this.openedCard.value2) {
+            alert("You win!");
+
+            this.cards.forEach((card) => {
+              card.isFlipped = false;
+            });
+
+            this.openedCard.value1 = null;
+            this.openedCard.value2 = null;
+          } else {
+            this.cards.forEach((card) => {
+              card.isFlipped = false;
+            });
+            this.openedCard.value1 = null;
+            this.openedCard.value2 = null;
+          }
+        }, 300);
+      }
+    },
+  },
+};
+</script>
